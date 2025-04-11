a36e4be1b40a938827fd29d06f32af23d93581da
sghersick1 <sghersick@loyola.edu>
2025-04-11 19:45:40 -0400

    finalized preprocessing

--- prep_split_data.py
+++ prep_split_data.py
@@ -15,16 +15,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-#read in the data set into a dataframe
-df = pd.read_csv("diabetes_binary.csv", encoding="utf-8")
+#load in the diabetes data, preprocess, return the split data
+def load_prep_split():
+    #read in the data set into a dataframe
+    df = pd.read_csv("diabetes_binary.csv", encoding="utf-8")
 
-#print the features and drop the 'ID' column
-print(f"The features are:\n{", ".join(df.columns)}")
-df.drop(columns=['ID'], inplace=True)
+    #print the features and drop the 'ID' column
+    print(f"The features are:\n{", ".join(df.columns)}")
+    df.drop(columns=['ID'], inplace=True)
 
-#check if the data is numeric
-print("\n", df.dtypes)
+    #check if the data is numeric
+    print("\n", df.dtypes)
 
-X = df.drop(columns=['Diabetes_binary'])
-y = df['Diabetes_binary']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1, stratify=y)
+    X = df.drop(columns=['Diabetes_binary'])
+    y = df['Diabetes_binary']
+    return train_test_split(X, y, test_size=0.25, random_state=1, stratify=y)
+
+#main method
+if __name__ == "__main__":
+    load_prep_split()
