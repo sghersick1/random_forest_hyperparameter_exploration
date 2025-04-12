d65c2a2cb5d077bf237819f65f776a03e0247cad
sghersick1 <sghersick@loyola.edu>
2025-04-11 21:30:04 -0400

    Finished the training for f1

--- prep_split_data.py
+++ prep_split_data.py
@@ -11,8 +11,6 @@ notes:
 
 #imports
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 #load in the diabetes data, preprocess, return the split data
